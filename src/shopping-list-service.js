151c60923465aef77048625e9f64494718157caa
julio-crypt0 <hernandezjulio58@ymail.com>
2019-11-06 09:17:58 -0800

    replicated functions from assignment

--- src/shopping-list-service.js
+++ src/shopping-list-service.js
@@ -2,22 +2,32 @@ const shoppingListService = {
   getAllItems(knex) {
     return knex.select('*').from('shopping_list');
   },
-  // getItemById(knex,id) {
-  //     return knex
-  //     .from('shopping-list')
-  // }
-  // deleteItemById(knex,id){
-  //     return knex
-  //     .from('shopping-list')
-  // }
-  // updateItemById(knex,id){
-  //     return knex
-  //     .from('shopping-list')
-  // }
-  // insertItem(knex,id){
-  //     return knex
-  //     .from('shopping-list')
-  // }
+  getItemById(knex, id) {
+    return knex
+      .from('shopping-list')
+      .select('*')
+      .where({ id })
+      .first();
+  },
+  deleteItemById(knex, id) {
+    return knex
+      .from('shopping-list')
+      .where({ id })
+      .delete();
+  },
+  updateItemById(knex, id, newData) {
+    return knex
+      .from('shopping-list')
+      .where({ id })
+      .update(newData);
+  },
+  insertItem(knex, item) {
+    return knex
+      .insert(item)
+      .into('shopping-list')
+      .returning('*')
+      .then((rows) => rows[0]);
+  },
 };
 
 module.exports = shoppingListService;
--- test/shopping-list-service.spec.js
+++ test/shopping-list-service.spec.js
@@ -1,6 +1,14 @@
 const shoppingListService = require('../src/shopping-list-service');
 
 describe('Shopping list service service object', function() {
+  let db;
+
+  before(() => {
+    db = knex({
+      client: 'pg',
+      connection: process.env.TEST_DB_URL,
+    });
+  });
   describe('Shopping list service object', () => {
     it('resolves all items from shopping-list table', () => {});
   });
